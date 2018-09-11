672525fcd5c019a66683ca9ec756621364f09032
Bruno Romero de Azevedo <brunordea@gmail.com>
2018-09-10 23:15:51 -0300

    Accept a JSON request to /paste path

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 extern crate actix_web;
+extern crate futures;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
 use actix_web::server;
 
 mod mypaste {
-    use actix_web::{App, HttpRequest};
+    use actix_web::{http, App, AsyncResponder, Error, HttpMessage, HttpRequest, HttpResponse};
+    use futures::future::Future;
 
-    fn paste(_req: &HttpRequest) -> String {
-        "Hello Paste!".to_owned()
+    #[derive(Debug, Serialize, Deserialize)]
+    struct PasteContent {
+        content: String,
+    }
+
+    fn paste(req: &HttpRequest) -> Box<Future<Item = HttpResponse, Error = Error>> {
+        req.json()
+            .from_err() // convert all errors into Error
+            .and_then(|content: PasteContent| {
+                println!("request content: {:?}", content);
+                Ok(HttpResponse::Ok().json(PasteContent {
+                    content: "Worked!".to_string(),
+                }))
+            }).responder()
     }
 
     pub(crate) fn app() -> App {
-        App::new().prefix("/").resource("/paste", |r| r.f(paste))
+        App::new().resource("/paste", |r| r.method(http::Method::POST).f(paste))
     }
 }
 
